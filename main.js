f909a4d248fa0700e9295693c4cf8bc5e1842c2b
Zeyad Ashraf <115799167+zeyad810@users.noreply.github.com>
2024-02-11 20:30:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let str ="Cademy";
+let count =7 ;
+let separator="_";
+let output ="";
+for(let start =0 ; start<count -1; start++ ){
+    output+= `${str} ${separator} `
+}
+output+= ` ${str}` 
+console.log(output)
